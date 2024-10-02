fed41369f275cfcca9c5009856ef5ae1b1356bea
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2024-10-02 12:46:02 +0300

    implement batch gradient descent

--- linear-regression.js
+++ linear-regression.js
@@ -17,22 +17,35 @@ class LinearRegression {
     this.weights = tf.zeros([this.features.shape[1], 1]);
   }
 
-  gradientDescent() {
-    const currentGuesses = this.features.matMul(this.weights);
+  gradientDescent(features, labels) {
+    const currentGuesses = features.matMul(this.weights);
 
-    const differences = currentGuesses.sub(this.labels);
+    const differences = currentGuesses.sub(labels);
 
-    const slopes = this.features
+    const slopes = features
       .transpose()
       .matMul(differences)
-      .div(this.features.shape[0]);
+      .div(features.shape[0]);
 
     this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
+    const batchQuantity = Math.floor(
+      this.features.shape[0] / this.options.batchSize
+    );
+
     for (let i = 0; i < this.options.iterations; i++) {
-      this.gradientDescent();
+      for (let j = 0; j < batchQuantity; j++) {
+        const { batchSize } = this.options;
+        const startIndex = j * batchSize;
+        const featuresSlice = this.features.slice(
+          [startIndex, 0],
+          [batchSize, -1]
+        );
+        const labelsSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+        this.gradientDescent(featuresSlice, labelsSlice);
+      }
       this.recordMSE();
       this.updateLearningRate();
     }
