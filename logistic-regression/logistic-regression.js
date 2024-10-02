ef794301e6822b62b9e76f0bbd81afbf32a2c3fe
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2024-10-02 14:56:03 +0300

    make decisionBoundary configurable

--- logistic-regression/logistic-regression.js
+++ logistic-regression/logistic-regression.js
@@ -10,6 +10,7 @@ class LogisticRegression {
       {
         learningRate: 0.1,
         iterations: 100,
+        decisionBoundary: 0.5,
       },
       options
     );
@@ -52,11 +53,15 @@ class LogisticRegression {
   }
 
   predict(observations) {
-    return this.processFeatures(observations).matMul(this.weights).sigmoid();
+    return this.processFeatures(observations)
+      .matMul(this.weights)
+      .sigmoid()
+      .greater(this.options.decisionBoundary)
+      .cast("float32");
   }
 
   test(testFeatures, testLabels) {
-    const predictions = this.predict(testFeatures).round();
+    const predictions = this.predict(testFeatures);
     testLabels = tf.tensor(testLabels);
     const incorrect = predictions.sub(testLabels).abs().sum().arraySync();
 
