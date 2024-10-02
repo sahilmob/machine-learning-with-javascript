b29d6a1bf86a860d53fdeacab196494ab4210344
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-10-02 19:42:28 +0300

    convert labels

--- multinational-logistic-regression/index.js
+++ multinational-logistic-regression/index.js
@@ -1,4 +1,5 @@
 require("@tensorflow/tfjs-node");
+const _ = require("lodash");
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("../load-csv");
 const LogisticRegression = require("./logistic-regression");
@@ -7,16 +8,25 @@ const { features, labels, testFeatures, testLabels } = loadCSV(
   "../data/cars.csv",
   {
     dataColumns: ["horsepower", "displacement", "weight"],
-    labelColumns: ["passedemissions"],
+    labelColumns: ["mpg"],
     shuffle: true,
     splitTest: 50,
     converters: {
-      passedemissions: (v) => (v === "TRUE" ? 1 : 0),
+      mpg: (v) => {
+        const mpg = parseFloat(v);
+        if (mpg < 15) {
+          return [1, 0, 0];
+        } else if (mpg < 30) {
+          return [0, 1, 0];
+        } else {
+          return [0, 0, 1];
+        }
+      },
     },
   }
 );
 
-const regression = new LogisticRegression(features, labels, {
+const regression = new LogisticRegression(features, _.flatMap(labels), {
   learningRate: 0.5,
   iterations: 100,
   batchSize: 50,
@@ -24,10 +34,6 @@ const regression = new LogisticRegression(features, labels, {
 });
 
 regression.train();
-// regression
-//   .predict([
-//     [130, 307, 1.75],
-//     [88, 97, 1.065],
-//   ])
-//   .print();
-console.log(regression.test(testFeatures, testLabels));
+
+regression.predict([[215, 440, 2.15]]).print();
+// console.log(regression.test(testFeatures, testLabels));
