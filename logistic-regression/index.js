90d99914f8a2400fb2943f572ac6feee072a22d8
Sahil H. Mubaideen <Sahil.mubaideen@network.global>
2024-10-03 11:56:37 +0300

    encode labels

--- logistic-regression/index.js
+++ logistic-regression/index.js
@@ -4,8 +4,14 @@ const LogisticRegression = require("./logistic-regression");
 const mnist = require("mnist-data");
 const _ = require("lodash");
 
-const mnistData = mnist.training(0, 1);
+const mnistData = mnist.training(0, 10);
 
 const features = mnistData.images.values.map((image) => _.flatMap(image));
 
-console.log(features);
+const encodedLabels = mnistData.labels.values.map((v) => {
+  const arr = Array(10).fill(0);
+  arr[v] = 1;
+  return arr;
+});
+
+console.log(encodedLabels);
